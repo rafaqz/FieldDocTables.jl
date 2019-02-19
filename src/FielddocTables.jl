cc6dc382376587f4349af7592eecd25f9f7db257
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-19 17:01:33 +1100

    better naming scheme

--- src/FielddocTables.jl
+++ src/FielddocTables.jl
@@ -3,22 +3,20 @@ module FielddocTables
 using DocStringExtensions, PrettyTables
 import DocStringExtensions: Abbreviation, format
 
-export FIELDDOCTABLE
+export FielddocTable
 
-
-struct TypeFieldMetadata{L,T,F} <: Abbreviation 
+struct FielddocTable{L,T,F} <: Abbreviation 
     labels::L
     functions::T
     tableformat::F
     fenced::Bool
 end
 
-TypeFieldMetadata(labels::L, functions::T; tableformat=PrettyTableFormat(markdown), fenced=false) where {L,T} =
-    TypeFieldMetadata{L,T,typeof(tableformat)}(labels, functions, tableformat, fenced)
+FielddocTable(labels::L, functions::T; tableformat=PrettyTableFormat(markdown), fenced=false) where {L,T} =
+    FielddocTable{L,T,typeof(tableformat)}(labels, functions, tableformat, fenced)
 
-const FIELDDOCTABLE = TypeFieldMetadata
-
-function format(metaformat::TypeFieldMetadata, buf, doc)
+function format(doctable::FielddocTable, buf, doc)
+    println("Nooooooooooooooooooooooooooooooooooooooooow?????????")
     local docs = get(doc.data, :fields, Dict())
     local binding = doc.data[:binding]
     local object = Docs.resolve(binding)
@@ -26,21 +24,23 @@ function format(metaformat::TypeFieldMetadata, buf, doc)
     # an empty vector to be consistent with the behaviour on v0.6.
     local fields = isabstracttype(object) ? Symbol[] : fieldnames(object)
 
-
     if !isempty(fields)
 
 
         # Fieldnames and passed in functions
-        colnames = [:Field, metaformat.labels...]
-        data = hcat([fields...], ([string.(f(object))...] for f in metaformat.functions)...)
+        colnames = [:Field, doctable.labels...]
+        data = hcat([fields...], ([safestring.(f(object))...] for f in doctable.functions)...)
 
         fielddocs = []
         for field in fields
             if haskey(docs, field) && isa(docs[field], AbstractString)
                 for line in split(docs[field], "\n")
-                    push!(fielddocs, isempty(line) ? "" : "    ", rstrip(line))
+                    fielddoc = isempty(line) ? "" : rstrip(line)
                 end
+            else
+                fielddoc = ""
             end
+            push!(fielddocs, fielddoc)
         end
         if any(d -> d != "", fielddocs)
             data = hcat(data, fielddocs)
@@ -48,12 +48,15 @@ function format(metaformat::TypeFieldMetadata, buf, doc)
         end
 
         println(buf)
-        metaformat.fenced && println(buf, "```")
-        pretty_table(buf, data, colnames, metaformat.tableformat)
-        metaformat.fenced && println(buf, "```")
+        doctable.fenced && println(buf, "```")
+        pretty_table(buf, data, colnames, doctable.tableformat)
+        doctable.fenced && println(buf, "```")
         println(buf)
     end
     return nothing
 end
+
+safestring(::Nothing) = "nothing"
+safestring(s) = string(s)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,22 @@
 module TestFieldDocTables
-    using FielddocTables, FieldMetadata, PrettyTables, Test, Markdown
 
-    import FieldMetadata: @default, default, @limits, limits, @description, @redescription, description
+using FielddocTables, FieldMetadata, PrettyTables, Test, Markdown
 
-    """
-    This type tests if FIELDMETADATA is printed as a markdown table.
+import FieldMetadata: @default, default, @limits, limits, @description, @redescription, description
 
-    $(FIELDDOCTABLE((:Description, :Default, :Limits), (description, default, limits)))
-    """
-    @description @limits @default mutable struct TestStruct
-       " A field doc"
-       a::Int     | 2   | (1, 10)     | "an Int"
-       b::Float64 | 4.0 | (2.0, 20.0) | "a Float"
-    end
+const FIELDDOCTABLE = FielddocTable((:Description, :Default, :Limits), (description, default, limits))
 
-    @test Markdown.plain(@doc TestStruct) == "This type tests if FIELDMETADATA is printed as a markdown table.\n\n| Field | Description | Default |      Limits |        Docs |\n| -----:| -----------:| -------:| -----------:| -----------:|\n|     a |      an Int |       2 |     (1, 10) |             |\n|     b |     a Float |     4.0 | (2.0, 20.0) | A field doc |\n"
+"""
+This type tests if FIELDMETADATA is printed as a markdown table.
+
+$(FIELDDOCTABLE)
+"""
+@description @limits @default mutable struct TestStruct
+   " A field doc"
+   a::Int     | 2   | (1, 10)     | "an Int"
+   b::Float64 | 4.0 | (2.0, 20.0) | "a Float"
+end
+
+@test Markdown.plain(@doc TestStruct) == "This type tests if FIELDMETADATA is printed as a markdown table.\n\n| Field | Description | Default |      Limits |        Docs |\n| -----:| -----------:| -------:| -----------:| -----------:|\n|     a |      an Int |       2 |     (1, 10) | A field doc |\n|     b |     a Float |     4.0 | (2.0, 20.0) |             |\n"
+
 end
