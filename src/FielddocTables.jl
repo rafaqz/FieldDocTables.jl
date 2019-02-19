1d3221cd2bb04b46a72432950f5c3ca5df9c9953
Rafael Schouten <rafaelschouten@gmail.com>
2019-02-19 14:09:10 +1100

    init repo

---
+++ src/FielddocTables.jl
@@ -0,0 +1,59 @@
+module FielddocTables
+
+using DocStringExtensions, PrettyTables
+import DocStringExtensions: Abbreviation, format
+
+export FIELDMETADATA
+
+
+struct TypeFieldMetadata{L,T,F} <: Abbreviation 
+    labels::L
+    functions::T
+    tableformat::F
+    fenced::Bool
+end
+
+TypeFieldMetadata(labels::L, functions::T; tableformat=PrettyTableFormat(markdown), fenced=false) where {L,T} =
+    TypeFieldMetadata{L,T,typeof(tableformat)}(labels, functions, tableformat, fenced)
+
+const FIELDMETADATA = TypeFieldMetadata
+
+function format(metaformat::TypeFieldMetadata, buf, doc)
+    local docs = get(doc.data, :fields, Dict())
+    local binding = doc.data[:binding]
+    local object = Docs.resolve(binding)
+    # On 0.7 fieldnames() on an abstract type throws an error. We then explicitly return
+    # an empty vector to be consistent with the behaviour on v0.6.
+    local fields = isabstracttype(object) ? Symbol[] : fieldnames(object)
+
+
+    if !isempty(fields)
+
+
+        # Fieldnames and passed in functions
+        colnames = [:Field, metaformat.labels...]
+        data = hcat([fields...], ([f(object)...] for f in metaformat.functions)...)
+
+        fielddocs = []
+        for field in fields
+            if haskey(docs, field) && isa(docs[field], AbstractString)
+                for line in split(docs[field], "\n")
+                    push!(fielddocs, isempty(line) ? "" : "    ", rstrip(line))
+                end
+            end
+        end
+        if any(d -> d != "", fielddocs)
+            data = hcat(data, fielddocs)
+            colnames = [colnames..., :Docs]
+        end
+
+        println(buf)
+        metaformat.fenced && println(buf, "```")
+        pretty_table(buf, data, colnames, metaformat.tableformat)
+        metaformat.fenced && println(buf, "```")
+        println(buf)
+    end
+    return nothing
+end
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,19 @@
+module TestFieldDocTables
+    using FielddocTables, FieldMetadata, PrettyTables, Test, Markdown
+
+    import FieldMetadata: @default, default, @limits, limits, @description, @redescription, description
+
+    """
+    This type tests if FIELDMETADATA is printed as a markdown table.
+
+    $(FIELDMETADATA((:Description, :Default, :Limits), (description, default, limits), 
+                    tableformat=PrettyTableFormat(markdown), fenced=false))
+    """
+    @description @limits @default mutable struct TestStruct
+       " A field doc"
+       a::Int     | 2   | (1, 10)     | "an Int"
+       b::Float64 | 4.0 | (2.0, 20.0) | "an untyped field"
+    end
+
+    @test Markdown.plain(@doc TestStruct) == "This type tests if FIELDMETADATA is printed as a markdown table.\n\n| Field |      Description | Default |      Limits |        Docs |\n| -----:| ----------------:| -------:| -----------:| -----------:|\n|     a |           an Int |     2.0 |     (1, 10) |             |\n|     b | an untyped field |     4.0 | (2.0, 20.0) | A field doc |\n"
+end
