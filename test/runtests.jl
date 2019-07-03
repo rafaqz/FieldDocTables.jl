cf968b41d88fcf469a8ba7d004769bf61b52cccc
Rafael Schouten <rafaelschouten@gmail.com>
2019-07-04 01:12:13 +1000

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,6 @@ $(FIELDDOCTABLE)
    b::Float64 | 4.0 | (2.0, 20.0) | "a Float"
 end
 
-@test Markdown.plain(@doc TestStruct) == "This type tests if FIELDMETADATA is printed as a markdown table.\n\n| Field | Description | Default |      Limits |        Docs |\n| -----:| -----------:| -------:| -----------:| -----------:|\n|     a |      an Int |       2 |     (1, 10) | A field doc |\n|     b |     a Float |     4.0 | (2.0, 20.0) |             |\n"
+@test Markdown.plain(@doc TestStruct) == "This type tests if FIELDMETADATA is printed as a markdown table.\n\n| Field | Description | Default |      Bounds |        Docs |\n| -----:| -----------:| -------:| -----------:| -----------:|\n|     a |      an Int |       2 |     (1, 10) | A field doc |\n|     b |     a Float |     4.0 | (2.0, 20.0) |             |\n"
 
 end
